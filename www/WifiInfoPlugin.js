badfa864d6c71c934cd606a4fc560ee3591d0608
raffaelebotta <raffaele.botta@greenisnow.com>
2013-11-29 18:00:51 +0100

    Update and rename calendar.js to WifiInfoPlugin.js

--- www/WifiInfoPlugin.js
+++ www/WifiInfoPlugin.js
@@ -1,3 +1,19 @@
+var WifiInfo= function() {
+};
+
+WifiInfo.prototype.get = function(success, fail) {
+                PhoneGap.exec(success, success, 'WifiInfoPlugin', null, [] );
+};
+
+cordova.addConstructor(function() {
+
+        if (!window.plugins) {
+                window.plugins = {};
+        }
+        window.plugins.WifiInfo = new WifiInfo();
+});
+
+/*
 var calendar = {
     createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
         cordova.exec(
@@ -16,3 +32,4 @@ var calendar = {
     }
 }
 module.exports = calendar;
+*/
